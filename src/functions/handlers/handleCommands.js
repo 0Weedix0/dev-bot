97df49ebf717cce373d96e6ac21551cd75f7fe16
0Weedix0 <121789488+0Weedix0@users.noreply.github.com>
2023-08-06 00:03:24 +0200

    bot v1

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -3,32 +3,31 @@ const fs = require('fs')
 const { token } = require('../../../config.json')
 
 module.exports = (client) => {
-    client.handleCommands = async() => {
+    client.handleCommands = async () => {
 
         const commandFolder = fs.readdirSync('./Commands')
-        for (const folder of commandFolder){
+        for (const folder of commandFolder) {
             const commandFile = fs.readdirSync(`./Commands/${folder}`).filter((file) => file.endsWith('.js'))
 
-            const {commands, commandArray} = client
-            for (const file of commandFile){
+            const { commands, commandArray } = client
+            for (const file of commandFile) {
                 const command = require(`../../../Commands/${folder}/${file}`)
                 commands.set(command.data.name, command)
                 commandArray.push(command.data.toJSON())
+                console.log(`${file}`)
             }
         }
-        
+
         const clientID = '1125816918192947262'
         const rest = new REST({ version: '10' }).setToken(token)
 
         try {
             console.log(`Started Refreshing Commands`)
 
-            await rest.put(
-                // Routes.applicationCommands(clientID, guildID), // This Will Work For Specific Guild(Server)
+         rest.put(
                 Routes.applicationCommands(clientID), // This Will Work For Multi Guild(Server)
-                { body: client.commandArray }
+                { body: client.commandArray },
             )
-
             console.log(`Successfully Refreshed Commands`)
         } catch (error) {
             console.error(error)
